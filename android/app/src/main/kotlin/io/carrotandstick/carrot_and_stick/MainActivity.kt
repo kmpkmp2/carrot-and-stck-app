2ab97d1078f5f5693932b1b0c71729bc5552724b
Dojun Kim <ehwns4855@gmail.com>
2019-11-25 20:17:51 +0900

    [INIT] 프로젝트 초기화

---
+++ android/app/src/main/kotlin/io/carrotandstick/carrot_and_stick/MainActivity.kt
@@ -0,0 +1,13 @@
+package io.carrotandstick.carrot_and_stick
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
