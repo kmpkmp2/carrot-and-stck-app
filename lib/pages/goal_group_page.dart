722a4d8f3213df477a6dfe678cf194301394f39f
Dojun Kim <ehwns4855@gmail.com>
2019-11-26 21:04:40 +0900

    [INIT] goal group page 생성

---
+++ lib/pages/goal_group_page.dart
@@ -0,0 +1,30 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class GoalGroupActivity extends StatefulWidget{
+
+  @override
+  GoalGroupActivityState createState() => GoalGroupActivityState();
+}
+
+class GoalGroupActivityState  extends State<GoalGroupActivity>
+{
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:AppBar(title: Text("GoalGroupActivity")),
+      body: Center(
+        child: Column(
+
+        ),
+      )
+    );
+  }
+}
