6ba12ac50ef0f56cb5153e1a78c16f4446030702
Dojun Kim <ehwns4855@gmail.com>
2019-11-27 22:38:22 +0900

    [ADD] textfield 추가해봄

--- lib/pages/goal_group_page.dart
+++ lib/pages/goal_group_page.dart
@@ -1,30 +1,41 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class GoalGroupActivity extends StatefulWidget{
-
+class GoalGroupActivity extends StatefulWidget {
   @override
   GoalGroupActivityState createState() => GoalGroupActivityState();
 }
 
-class GoalGroupActivityState  extends State<GoalGroupActivity>
-{
-
+class GoalGroupActivityState extends State<GoalGroupActivity> {
   @override
   void initState() {
     super.initState();
   }
 
+  TextEditingController titleController;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(title: Text("GoalGroupActivity")),
-      body: Center(
-        child: Column(
+        appBar: AppBar(title: Text("목표 그룹 생성")),
+        body: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+               Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Flexible(
+                     child: TextField(
+                       controller: titleController,
+                       decoration: InputDecoration(
+                         hintText: "목표 명"
+                       ),
+                     ),
+                    )
+                  ],
 
-        ),
-      )
-    );
+              )
+            ])));
   }
 }
