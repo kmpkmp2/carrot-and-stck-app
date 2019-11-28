5ceb12b1239645614b1250a51f75b975961e679b
Dojun Kim <ehwns4855@gmail.com>
2019-11-28 23:48:50 +0900

    [Add] TextField  목표 설명, 최소 배팅금액 추가

--- lib/pages/goal_group_page.dart
+++ lib/pages/goal_group_page.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -13,6 +15,8 @@ class GoalGroupActivityState extends State<GoalGroupActivity> {
   }
 
   TextEditingController titleController;
+  TextEditingController descriptionController;
+  TextEditingController minimumBetController;
 
   @override
   Widget build(BuildContext context) {
@@ -20,22 +24,22 @@ class GoalGroupActivityState extends State<GoalGroupActivity> {
         appBar: AppBar(title: Text("목표 그룹 생성")),
         body: Center(
             child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-               Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Flexible(
-                     child: TextField(
-                       controller: titleController,
-                       decoration: InputDecoration(
-                         hintText: "목표 명"
-                       ),
-                     ),
-                    )
-                  ],
-
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+              TextField(
+                controller: titleController,
+                decoration: InputDecoration(hintText: "목표 명"),
+              ),
+              TextField(
+                controller: descriptionController,
+                decoration: InputDecoration(hintText: "목표 설명"),
+              ),
+              TextField(
+                controller: minimumBetController,
+                decoration: InputDecoration(hintText: "최소 배팅금액"),
               )
-            ])));
+          ],
+        )
+    ));
   }
 }
